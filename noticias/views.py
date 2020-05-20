693aa7b1b4f2ca1aa32781c1ebed6cf57602c824
ViniciusGabrielOliveira <viniciusgabriel.oliveira@gmail.com>
2020-05-20 15:13:29 -0300

    control access auth

--- noticias/views.py
+++ noticias/views.py
@@ -7,15 +7,25 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import filters
-
-
+from rest_framework.permissions import AllowAny
 from noticias.serializers import UserSerializer, GroupSerializer, NoticiaSerializer
 from noticias.models import Noticia
+from noticias.permissions import IsAdminUser, IsLoggedInUserOrAdmin
 
 
 class NoticiaViewSet(viewsets.ModelViewSet):
     queryset = Noticia.objects.all()
     serializer_class = NoticiaSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.action == 'list':
+            permission_classes = [AllowAny]
+        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update' or self.action == 'create':
+            permission_classes = [IsLoggedInUserOrAdmin]
+        elif self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 class DetalheList(generics.ListAPIView):
     queryset = Noticia.objects.all()
